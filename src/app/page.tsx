d6b7aef28cac051365918d384f65779e073c8d9c
rathore-07 <yash.rathore7603@gmail.com>
2024-05-19 23:25:07 +0530

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { ImGithub } from "react-icons/im";
 export default function Home() {
   return (
     <main className="flex w-full justify-center items-center p-24">
-      <div className="font-bold text-5xl mr-5"> - Github Actions - </div>
+      <div className="font-bold text-5xl mr-5"> Github Actions </div>
       <ImGithub size={50} />
     </main>
   );
