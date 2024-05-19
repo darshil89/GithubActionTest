01bde6d8d1142ebd0e4b6adc6040674c74d75e36
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-05-19 11:29:34 +0530

    added demo project for testing github actions

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { ImGithub } from "react-icons/im";
+
+export default function Home() {
+  return (
+    <main className="flex w-full justify-center items-center p-24">
+      <div className="font-bold text-5xl mr-5">Github Actions</div>
+      <ImGithub size={50} />
+    </main>
+  );
+}
